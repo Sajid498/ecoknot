bf757c0afb4beec8a3974002fa5a25960bf1b6e1
Shahriar <shahriarsajid888@gmail.com>
2026-09-19 23:36:22 +0600

    Add expired status and enable scheduler

--- backend/src/main/java/backend/entity/RequestStatus.java
+++ backend/src/main/java/backend/entity/RequestStatus.java
@@ -3,12 +3,23 @@ package backend.entity;
 
 public enum RequestStatus {
 
+
     OPEN,
+
 
     DONOR_FOUND,
 
+
     FULFILLED,
 
-    CANCELLED
+
+    CANCELLED,
+
+
+    // Phase 4.2
+    // Blood request expired because required date passed
+
+    EXPIRED
+
 
 }
